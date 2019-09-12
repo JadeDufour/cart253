f0f7845294d0fab512f4e62641d3e624277bf85e
Jade <jade1dufour@gmail.com>
2019-09-12 16:36:01 -0400

    starting stage

---
+++ exercice/exercise 0/js/script.js
@@ -0,0 +1,42 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+"use strict";
+let x=0;
+let y=0;
+let speed;
+let circleColor=250;
+
+function setup() {
+  createCanvas(500,500);
+  x = width/2;
+  y = height/2;
+  speed = random(1,10);
+  let circleColor = random(255);
+  fill(circleColor);
+}
+function draw() {
+  x += speed;
+  console.log("x is:" + x )
+  let circleSize = 50;
+  ellipse(x,y,circleSize,circleSize);
+}
