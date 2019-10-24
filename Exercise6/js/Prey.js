19f57709fdf335197c154e51617ae8aa7cfd1b01
JadeDufour <jade1dufour@gmail.com>
2019-10-24 15:24:01 -0400

    Exe6: Prey: There was an extra R

--- Exercise6/js/Prey.js
+++ Exercise6/js/Prey.js
@@ -37,7 +37,7 @@ class Prey { ////////////////////FIXED (Changed Glass for Class)
   //
   // Sets velocity based on the noise() function and the Prey's speed
   // Moves based on the resulting velocity and handles wrapping
-  mover() {
+  move() {  ////////////////////FIXED (There was an extra R)
     // Set velocity via noise()
     this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
     this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
