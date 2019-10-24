22794de43f4c377a7bf51cc7d93c6fb3dd11e970
JadeDufour <jade1dufour@gmail.com>
2019-10-24 15:35:59 -0400

    Exe6: Predator : Up, not App

--- Exercise6/js/Predator.js
+++ Exercise6/js/Predator.js
@@ -31,7 +31,7 @@ class Predator {
     this.fillColor = fillColor;
     this.radius = this.health; // Radius is defined in terms of health ////////////////////FIXED (Changed the letter O for U)
     // Input properties
-    this.appKey = UP_ARROW;
+    this.upKey = UP_ARROW; ////////////////////FIXED (UP, not App)
     this.downKey = DOWN_ARROW;
     this.leftKey = LEFT_ARROW;
     this.rightKey = RIGHT_ARROW;
