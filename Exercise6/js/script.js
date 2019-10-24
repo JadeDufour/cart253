8ae29a48f5f856adfbc071e8b6ae919b184a42c4
JadeDufour <jade1dufour@gmail.com>
2019-10-24 15:48:44 -0400

    Exe6: Script Missing speed for the zebra

--- Exercise6/js/script.js
+++ Exercise6/js/script.js
@@ -27,7 +27,7 @@ function setup() { ////////////////////FIXED (Function and Setup must be two sep
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40); ////////////////////FIXED (There was one extra comma)
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 8, color(255, 255, 255), 60);
+  zebra = new Prey(100, 8, 10,  color(255, 255, 255), 60); ////////////////////FIXED (Missing speed for the zebra)
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
 
